459dc0a918add1f8a5475444fa4f0119eb0bcc35
CarlosEduardoNovaes <carlosnov@gmail.com>
2015-04-12 19:04:00 -0300

    Update read_bin_xps.py

--- XNALaraMesh/read_bin_xps.py
+++ XNALaraMesh/read_bin_xps.py
@@ -2,6 +2,7 @@
 
 import io
 import os
+import ntpath
 
 from XNALaraMesh import bin_ops
 from XNALaraMesh import read_ascii_xps
@@ -230,7 +231,7 @@ def readMeshes(file, xpsHeader, hasBones):
         textures = []
         textureCount = bin_ops.readUInt32(file)
         for texId in range(textureCount):
-            textureFile = os.path.basename(readFilesString(file))
+            textureFile = ntpath.basename(readFilesString(file))
             # print('Texture file', textureFile)
             uvLayerId = bin_ops.readUInt32(file)
 
